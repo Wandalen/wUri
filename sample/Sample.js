e3a600406ce5830a35bb4b52135c5fa59021ec48
Dmytro <dm.vict.kr@gmail.com>
2020-03-05 09:29:27 +0200

    Create template of module, same description in 4 files

--- sample/Sample.js
+++ sample/Sample.js
@@ -1,7 +1,46 @@
 
-let _ = require( 'wTools' );
+if( typeof module !== 'undefined' )
+require( 'wuri' );
+var _ = wTools;
 
-/**/
+console.log( '= Uri' );
+var uri = "git://../repo/Tools?out=out/wTools.out.will#master"
+var parsed = _.uri.parseAtomic( uri );
+console.log( 'Uri : ', uri );
+console.log( 'Parsed ( atomic ) :\n', parsed );
 
-...
+/* log
+Parsed ( atomic ) :
+ [Object: null prototype] {
+  'protocol' : 'git',
+  'host' : '..',
+  'localWebPath' : '/repo/Tools',
+  'query' : 'out=out/wTools.out.will',
+  'hash' : 'master'
+}
+*/
 
+/* */
+
+console.log( '= WebUri' );
+var uri = 'complex+protocol://www.site.com:13/path/name?query=here&and=here#anchor';
+var parsed = _.weburi.parse( uri );
+console.log( 'Uri : ', uri );
+console.log( 'Parsed ( full ) : \n', parsed );
+
+/* log 
+Parsed ( full ) :
+ [Object: null prototype] {
+  'protocol' : 'complex+protocol',
+  'host' : 'www.site.com',
+  'port' : '13',
+  'localWebPath' : '/path/name',
+  'query' : 'query=here&and=here',
+  'hash' : 'anchor',
+  'longPath' : 'www.site.com:13/path/name',
+  'protocols' : [ 'complex', 'protocol' ],
+  'hostWithPort' : 'www.site.com:13',
+  'origin' : 'complex+protocol://www.site.com:13',
+  'full' : 'complex+protocol://www.site.com:13/path/name?query=here&and=here#anchor'
+}
+*/
